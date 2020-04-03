2d2c36afb414ee78417f7a8b9981bd21c62fe871
steve-johansson <stephan.jansen@web.de>
2020-04-03 12:28:19 +0200

    copied files from stephengrider

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t stevejohansson/multi-client:latest -t stevejohansson/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t stevejohansson/multi-server:latest -t stevejohansson/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t stevejohansson/multi-worker:latest -t stevejohansson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push stevejohansson/multi-client:latest
 docker push stevejohansson/multi-server:latest
 docker push stevejohansson/multi-worker:latest
@@ -8,6 +9,7 @@ docker push stevejohansson/multi-worker:latest
 docker push stevejohansson/multi-client:$SHA
 docker push stevejohansson/multi-server:$SHA
 docker push stevejohansson/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stevejohansson/multi-server:$SHA
 kubectl set image deployments/client-deployment client=stevejohansson/multi-client:$SHA
